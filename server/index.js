aa0fcbb6fb4b508edd3c5ad48e9f0877b58cf095
Your Name <you@example.com>
2021-04-30 16:17:37 -0600

    Created database inside my mysql database.

--- server/index.js
+++ server/index.js
@@ -16,6 +16,12 @@ const con = mysql.createConnection({
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log("Database created.");
+  });
 });
 
 app.listen(port, () => {
