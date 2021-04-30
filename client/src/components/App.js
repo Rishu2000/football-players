0520b3be352831f50137dc50417d3b778beecd33
Your Name <you@example.com>
2021-04-30 11:15:06 -0600

    Removed styled-component and Installed styled-components, there is difference.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import "../styles/App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Register>Register</Register>
+      <LogIn>Login</LogIn>
+    </div>
+  );
 };
 
 export default App;
+
+const Register = styled.div``;
+
+const LogIn = styled.div``;
