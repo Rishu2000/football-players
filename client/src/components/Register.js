43d615d13b769fa6c932d9f040e39535b94c40e5
Your Name <you@example.com>
2021-04-30 13:42:04 -0600

    Completed Registration front-end part.

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,14 +2,81 @@ import React from "react";
 import styled from "styled-components";
 
 const Register = () => {
-  return <Container>Register</Container>;
+  return (
+    <Container>
+      <Content>
+        <Text>Register</Text>
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputFirstName">First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputFirstName"
+              placeholder="Enter First Name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputLastName">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputLastName"
+              placeholder="Enter Last Name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputContactNo">Contact Number</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputContactNo"
+              placeholder="Contact Number"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputContactNo">
+              Name of football club/country you support
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputClub"
+              placeholder="Club/Country Name"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </Content>
+    </Container>
+  );
 };
 
 export default Register;
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* border: 1px solid black; */
+const Container = styled.div``;
+
+const Content = styled.div`
+  width: 100%;
+`;
+
+const Text = styled.div`
+  font-weight: 800;
+  font-size: 40px;
+  text-align: center;
 `;
