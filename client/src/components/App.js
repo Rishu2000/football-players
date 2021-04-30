7dafcf2039ca74094674625e39a5ed16287c235f
Your Name <you@example.com>
2021-04-30 11:27:34 -0600

    Created seperated components for Login and Register.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,26 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
+import Register from "../components/Register";
+import LogIn from "../components/LogIn";
 import "../styles/App.css";
 
 const App = () => {
   return (
-    <div>
-      <Register>Register</Register>
-      <LogIn>Login</LogIn>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6">
+          <Register />
+        </div>
+        <div className="col-6">
+          <LogIn />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default App;
 
-const Register = styled.div``;
+// const Register = styled.div``;
 
-const LogIn = styled.div``;
+// const LogIn = styled.div``;
