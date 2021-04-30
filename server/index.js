842b0fb880aeb941a63d2cee42fa9979cac02d18
Your Name <you@example.com>
2021-04-30 15:01:40 -0600

    Basic Express code.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("object");
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+app.listen(port, () => {
+  console.log("server has been started in port " + port);
+});
