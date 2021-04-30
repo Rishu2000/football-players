7bca626c7c7b16bdecab528baff5eb5f2ca67279
Your Name <you@example.com>
2021-04-30 16:01:53 -0600

    Estabillised a connection and placed a function to identify it's connection.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.json("Hello");
+});
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
 app.listen(port, () => {
