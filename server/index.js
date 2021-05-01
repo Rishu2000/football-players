53f94b12258e43723055f141fc156928c5dc06ba
Your Name <you@example.com>
2021-04-30 21:26:23 -0600

    implemented cors middleware and get the data from "/register" url.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 const port = 5000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("Hello");
@@ -14,17 +18,16 @@ const con = mysql.createConnection({
   database: "mydb"
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+app.post("/register", (req, res) => {
+  const firstname = req.body.firstName;
+  const lastname = req.body.lastName;
+  const emailid = req.body.email;
+  const contactno = req.body.contactNo;
+  const cluborcountry = req.body.clubOrCountry;
+
   con.query(
-    "INSERT INTO users (firstName,lastName) VALUES ('Rishav','Kumar Shah')",
-    function (err, result) {
-      if (err) {
-        throw err;
-      }
-      console.log("Table created.");
-    }
+    "INSERT INTO users (firstName,lastName,emailAdd,contactNo,clubOrCounty) VALUES (?,?,?,?,?)",
+    [firstname, lastname, emailid, contactno, cluborcountry]
   );
 });
 
