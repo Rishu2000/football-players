47e18f4fb14176ff8b46a4bd1d3edb672267654e
Your Name <you@example.com>
2021-05-01 18:11:41 -0600

    Players file not completed yet.

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,16 +1,62 @@
 import React from "react";
+import styled from "styled-components";
 import TileCard from "../components/TileCard";
 
 const Players = () => {
   return (
-    <div>
-      <TileCard
-        name="Rishav Kumar Shah"
-        club="Bihar shafwewggeeg"
-        country="China"
-      />
-    </div>
+    <BigContainer>
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <Container>
+              <TileCard
+                name="Rishav Kumar Shah"
+                club="Bihar shafwewggeeg"
+                country="China"
+              />
+              <TileCard
+                name="Rishav Kumar Shah"
+                club="Bihar shafwewggeeg"
+                country="China"
+              />
+              <TileCard
+                name="Rishav Kumar Shah"
+                club="Bihar shafwewggeeg"
+                country="China"
+              />
+              <TileCard
+                name="Rishav Kumar Shah"
+                club="Bihar shafwewggeeg"
+                country="China"
+              />
+              <TileCard
+                name="Rishav Kumar Shah"
+                club="Bihar shafwewggeeg"
+                country="China"
+              />
+              <TileCard
+                name="Rishav Kumar Shah"
+                club="Bihar shafwewggeeg"
+                country="China"
+              />
+            </Container>
+          </div>
+          <div className="col-3"></div>
+        </div>
+      </div>
+    </BigContainer>
   );
 };
 
 export default Players;
+
+const BigContainer = styled.div`
+  height: 100%;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 33.3% 33.3% 33.3%;
+  grid-template-rows: 50% 50%;
+`;
