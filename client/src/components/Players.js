1160370a9f9d764f44ffc5f6f0d3976359edf3d5
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-05-03 07:15:18 +0530

    back to Zero and do it again here, Removed YourPlayers section.

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,9 +1,7 @@
 import React from "react";
-import {DragDropContext, Droppable} from "react-beautiful-dnd";
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 import styled from "styled-components";
 import TileCard from "../components/TileCard";
-
-const noOfEmptyTileCard = 3;
 
 const playersInfo = [
   {
@@ -40,43 +38,29 @@ const playersInfo = [
 const Players = () => {
   return (
     <Container>
-      <DragDropContext>
-        <Droppable droppableId="dropableID">
-          {(provided) => (
             <MiniContainer>
       <PlayersContainer>
+        <DiscContent>
         <Text>Players</Text>
+        <Content>Upper Three are default selected player's tile, you can select any others by draging and droping on upper three.</Content>
+        </DiscContent>
         <PlayersTile>
-          <ul {...provided.droppableProps} ref={provided.innerRef}>
+          <ul>     
             {
-              playersInfo.map(({name,club,country},key) => {
+              playersInfo.map(({name,club,country}) => {
                 return (
-                  <li key={key}>
+                  <EmptyTile>
+                  <li>  
                     <TileCard name={name} club={club} country={country}/> 
                   </li>
+                  </EmptyTile>
                 )
               })
             }
           </ul>
         </PlayersTile>
       </PlayersContainer>
-      <YourContainer>
-        <Text>Your Players</Text>
-        <YoursTile {...provided.droppableProps} ref={provided.innerRef}>
-          {
-            [...Array(noOfEmptyTileCard)].map((e,i) => {
-            return (
-                <EmptyTile>
-                </EmptyTile>
-              )
-          })
-          }
-        </YoursTile>
-      </YourContainer>
       </MiniContainer>
-      )}
-      </Droppable>
-      </DragDropContext>
     </Container>
   )
 };
@@ -90,20 +74,14 @@ const Container = styled.div`
 const MiniContainer = styled.span`
   height:100%;
   display:flex;
-  display:grid;
-  grid-template-columns:2fr 1fr;
 `
 
 const PlayersContainer = styled.div`
   display:grid;
-  grid-template-rows:60px 1fr;
+  grid-template-rows:auto 1fr;
 `
 
-const YourContainer = styled.div`
-  border-left:2px solid grey;
-  display:grid;
-  grid-template-rows:60px 1fr;
-`
+const DiscContent = styled.div``
 
 const Text = styled.div`
 text-align:center;
@@ -111,8 +89,13 @@ font-size:40px;
 font-weight:800;
 `
 
+const Content = styled.div`
+  text-align:center;
+`
+
 const PlayersTile = styled.div`
 border-top:2px solid gray;
+margin:0 80px;
   ul {
       height:100%;
       display:flex;
@@ -123,18 +106,9 @@ border-top:2px solid gray;
   }
 `
 
-const YoursTile = styled.div`
-height:100%;
-border-top:2px solid gray;
-display:flex;
-flex-direction:column;
-align-items:center;
-justify-content:space-evenly;
-`
-
 const EmptyTile = styled.div`
-  width:250px;
-  height:150px;
+  width:300px;
+  height:200px;
   border:1px solid black;
   border-radius:4px;
 `
